91c67203c48e048e6c7491818da9a552fefa6526
VictorManoel16 <vm20194546@gmail.com>
2025-10-06 13:21:11 -0300

    conexao.php

---
+++ config/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$DB_HOST = '127.0.0.1';
+$DB_NAME = 'projeto_login';
+$DB_USER = 'root';
+$DB_PASS = ''; 
+
+$dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    
+];
+
+try {
+    $pdo = new PDO($dsn, $DB_USER, $DB_PASS, $options);
+} catch (PDOException $e) {
+    
+    die('Erro ao conectar ao banco de dados: ' . $e->getMessage());
+}
